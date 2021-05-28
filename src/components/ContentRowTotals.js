3c0e6218c2633900744751639ed70c2c55cb5bba
Jaime Rivera <riverac85@gmail.com>
2021-05-28 17:56:27 -0500

    funcionalidad totales

--- src/components/ContentRowTotals.js
+++ src/components/ContentRowTotals.js
@@ -1,50 +1,107 @@
-import React from 'react';
+
 import SmallCard from './SmallCard';
+import React, { useState, useEffect} from 'react';
+import Category  from './Category';
+import axios from 'axios';
+import _ from "lodash";
 
-/*  Cada set de datos es un objeto literal */
 
-/* <!-- Movies in DB --> */
 
-let productsInDB = {
-    title: 'Productos',
-    color: 'primary', 
-    cuantity: 21,
-    icon: 'fa-dollar-sign'
-}
 
-/* <!-- Total awards --> */
 
-let totalUsers = {
-    title:'Usuarios', 
-    color:'success', 
-    cuantity: '79',
-    icon:'fa-users'
-}
 
-/* <!-- Actors quantity --> */
-
-let categories = {
-    title:'Categorias' ,
-    color:'warning',
-    cuantity:'49',
-    icon:'fa-cubes'
-}
-
-let cartProps = [productsInDB, totalUsers, categories];
 
 function ContentRowTotals(){
-    return (
+   
+    const [totals, setTotals] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+   
+    let catHijas = 0;
+
+    useEffect(() => {
+        axios({
+            url: 'http://localhost:3030/api/totals',
+            method: 'GET'
+          }).then((result) => {
+          
+            setTotals(result.data)
+            setIsLoaded(true); 
+
+          }
+            ).catch((error) => {
+                console.log(error);
+                setIsLoaded(true);   
+                setError(error);
+            } )
     
-        <div className="row">
-            
-            {cartProps.map( (panel, i) => {
+    }, [])
 
-                return <SmallCard {...panel} key={i}/>
-            
-            })}
 
-        </div>
-    )
+    if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded ) {
+        return <div>Loading...</div>;
+     } else if(totals != null ){
+        
+        console.log("totals");
+
+        console.log(totals);
+
+        let productsInDB = {
+            title: 'Productos',
+            color: 'primary', 
+            cuantity: totals.products,
+            icon: 'fa-dollar-sign'
+        }
+        
+        
+        let totalUsers = {
+            title:'Usuarios', 
+            color:'success', 
+            cuantity: totals.users,
+            icon:'fa-users'
+        }
+        
+        
+        let baseCategories = {
+            title:'Categorias Base' ,
+            color:'warning',
+            cuantity:totals.baseCategories,
+            icon:'fa-cubes'
+        }
+        
+        let childCategories = {
+            title:'Categorias Hijas' ,
+            color:'warning',
+            cuantity:totals.childCategories,
+            icon:'fa-cubes'
+        }
+        
+
+        let cartProps = [productsInDB, totalUsers, baseCategories, childCategories];
+
+
+        return (
+            <React.Fragment>
+        
+            <div className="row">
+            
+                {cartProps.map( (panel, i) => {
+
+                    return <SmallCard {...panel} key={i}/>
+                
+                })}
+
+              </div>
+            </React.Fragment>
+
+        );
+      }
+
+    
+ 
 }
 
 export default ContentRowTotals;
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function SmallCard(props){
+    
     return(
-        <div className="col-md-4 mb-4">
+        <div className="col-md-3 mb-4">
             <div className={`card border-left-${props.color} shadow h-100 py-2`}>
                 <div className="card-body">
                     <div className="row no-gutters align-items-center">
